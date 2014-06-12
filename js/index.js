f063f7b198fd9ae7cd60458a2a93825a9da004d8
fullhdpixel <tsmudde@hotmail.com>
2014-06-12 22:30:51 +0200

    AJAX

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
+function getInfo(str) {
+	if (str=="") {
+	location.reload();
+    return;
+	} 
+	if (window.XMLHttpRequest) {
+		xmlhttp=new XMLHttpRequest();
+	} else {
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange=function() {
+		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+			document.getElementById("json").innerHTML=xmlhttp.responseText;
+		}
+	}
+	xmlhttp.open("GET","./getAPI.php",true);
+	xmlhttp.send();
+}
+
+
 $(function(){
 	
 	// container is the DOM element;
