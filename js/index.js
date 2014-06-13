7b4566e948abfc6b1ec2134c3cc6702171979be5
fullhdpixel <tsmudde@hotmail.com>
2014-06-13 11:39:46 +0200

    DBcheck

--- js/index.js
+++ js/index.js
@@ -1,48 +1,25 @@
-window.setInterval(function(){
-  getInfo();
-}, 20000);
-
-function getInfo() {
-	if (window.XMLHttpRequest) {
-		xmlhttp=new XMLHttpRequest();
-	} else {
-		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	xmlhttp.onreadystatechange=function() {
-		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-			var json = xmlhttp.responseText;
-			var parsed = JSON.parse(json);
-			document.getElementById("last").innerHTML = parsed.Last;
-		}
-	}
-	xmlhttp.open("GET","./getAPI.php",true);
-	xmlhttp.send();
-}
+$(document).ready(function() {
 
 
-$(function(){
-	
-	// container is the DOM element;
-	// userText is the textbox
-	
-	var container = $(".price");
-	var	userText = $('#userText'); 
-	
-	// Shuffle the contents of container
-	container.shuffleLettersFast();	
-	
+	var container = $("#last");
+
 	window.setInterval(function(){
-	container.shuffleLettersSlow();	
-	}, 20000);
+		container.shuffleLetters();
+	}, 5000);
+
+	container.shuffleLetters();
+
+
 });
 
 var colors = new Array(
-  [30,170,226],
-  [12,115,146],
-  [171,207,58],
-  [253,205,17],//
-  [240,91,40],
-  [210,31,51]);
+	[30,170,226],
+	[12,115,146],
+	[171,207,58],
+	[253,205,17],
+	[240,91,40],
+	[210,31,51]
+);
 
 var step = 0;
 //color table indices for: 
@@ -55,41 +32,40 @@ var colorIndices = [0,1,2,3];
 //transition speed
 var gradientSpeed = 0.0005;
 
-function updateGradient()
-{
-var c0_0 = colors[colorIndices[0]];
-var c0_1 = colors[colorIndices[1]];
-var c1_0 = colors[colorIndices[2]];
-var c1_1 = colors[colorIndices[3]];
+function updateGradient() {
+	var c0_0 = colors[colorIndices[0]];
+	var c0_1 = colors[colorIndices[1]];
+	var c1_0 = colors[colorIndices[2]];
+	var c1_1 = colors[colorIndices[3]];
 
-var istep = 1 - step;
-var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
-var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
-var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-var color1 = "#"+((r1 << 16) | (g1 << 8) | b1).toString(16);
+	var istep = 1 - step;
+	var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+	var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+	var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+	var color1 = "#"+((r1 << 16) | (g1 << 8) | b1).toString(16);
 
-var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
-var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
-var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-var color2 = "#"+((r2 << 16) | (g2 << 8) | b2).toString(16);
+	var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+	var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+	var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+	var color2 = "#"+((r2 << 16) | (g2 << 8) | b2).toString(16);
 
- $('body').css({
-   background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
-    background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
-  
-  step += gradientSpeed;
-  if ( step >= 1 )
-  {
-    step %= 1;
-    colorIndices[0] = colorIndices[1];
-    colorIndices[2] = colorIndices[3];
-    
-    //pick two new target color indices
-    //do not pick the same as the current one
-    colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-    colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-    
-  }
+	$('body').css( {
+	   background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
+		background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
+	  
+	  step += gradientSpeed;
+	  if ( step >= 1 )
+	  {
+		step %= 1;
+		colorIndices[0] = colorIndices[1];
+		colorIndices[2] = colorIndices[3];
+		
+		//pick two new target color indices
+		//do not pick the same as the current one
+		colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+		colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+		
+	}
 }
 
 setInterval(updateGradient,10);
