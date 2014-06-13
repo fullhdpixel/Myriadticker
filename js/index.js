f13b1f2fd8506dbc07a1db233e110f7e915d8ac4
fullhdpixel <tsmudde@hotmail.com>
2014-06-13 22:32:47 +0200

    last commit
    over and out.

--- js/index.js
+++ js/index.js
@@ -7,24 +7,3 @@ $(document).ready(function() {
 		container.shuffleLetters();
 	}, 5000);
 });
-
-/*
-function currencySelected(currency) {
-	var btc = document.getElementById("showpairs").innerHTML;
-	
-	alert(btc);
-
-	if (currency == "") {
-		location.reload();
-		return;
-	} 
-	if (currency == "BITCOIN") {
-		var value = btc;
-	} else if (currency == "EURO") {
-		var value = btc * 500;
-	} else if (currency == "USD" ) {
-		var value = btc * 650;
-	}
-	document.getElementById("last").innerHTML = value;
-}
-*/
